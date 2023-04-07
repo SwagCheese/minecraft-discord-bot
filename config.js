cb67e0feeb41b4cdf504057fbd095174e424fc4a
bob <swagcheeseyt@gmail.com>
2023-04-06 19:53:45 -0700

    update config.js

--- config.js
+++ config.js
@@ -2,14 +2,14 @@ const config = {
   prefix: '!', // the symbol used when telling the discord bot to do something
   serverStartCommand: 'SERVER START COMMAND', // command to start the server (I recommended putting nogui at the end of the command to remove the server dashboard)
   serverStartTimeout: 300, // after this many seconds, the bot will give up on waiting for the server to start
-  serverIp: 'IP:PORT', // the server ip
-  token: 'MTA5MzY4MTUyMTU2NDU5ODMyMw.Gdp-5v.tx-PYY9bBxXdMYlPbzszdTo_NSDrZCQVKi6WsQ', // your bot's token
+  serverIp: 'IP:PORT', // the server ip and port
+  token: 'TOKEN', // your bot's token
   rcon_port: 25575, // the port that rcon is running on
   rcon_password: '1234', // the password used to connect to rcon
   backup_dir: 'C:/backup/directory', // the directory that your backup files will end up in
   dirs_to_backup: [ 'C:/serverDir/world', 'C:/serverDir/world_nether', 'C:/serverDir/world_the_end', ], // the directories that you want to back up
-  operators: [ '1234567890', '0987654321', ], // an array of user ids that can send commands to the server (not including start + stop)
+  operators: [ 'USER_ID' ], // an array of user ids that can send commands to the server (not including start + stop)
   // see https://youtu.be/ZPROrf4Fe3Q for more information on how to get users ids
 }
 
-module.exports=config; // don't edit this line
+module.exports = config; // don't edit this line

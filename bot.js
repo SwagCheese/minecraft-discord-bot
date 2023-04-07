913e95a16843bcae9ba2c52f0db473d920122195
bob <swagcheeseyt@gmail.com>
2023-04-06 20:01:43 -0700

    add error messages

--- bot.js
+++ bot.js
@@ -1,24 +1,3 @@
-/* By SwagCheese
- *
- * THIS SERVER MUST BE HOSTED ON YOUR LOCAL MACHINE FOR THIS TO WORK.
- * Try modifying this code using https://www.npmjs.com/package/selenium-webdriver if you are using a server hosting website
- *
- * SETUP:
- * 1: setup a minecraft server (https://youtu.be/7SClg1-GsLg) This tutorial should work for most versions
- * 2: set up a discord bot (https://youtu.be/E6Dg1miuW_k) Use a newer tutorial if this one is old
- * 3: once you get to the programming portion of the tutorial, paste this code into the file instead
- * 4: install the package minecraft-server-util (run the command npm install minecraft-server-util)
- * 5: in your minecraft server's server.properties file, find enable-rcon and set it to true
- * 6: (recommended) still in your server.properties file, find rcon-password and set a password to use
- * 7: edit the variables below to change your server ip, bot token, rcon-password etc.
- *
- * USAGE:
- * {prefix}start: starts the server
- * {prefix}stop: stops the server
- * {prefix}exec: executes a command on the server. Your user id must be in the operators array for this to work
- * {prefix}backup: makes a backup of the server to the specified directory. Your user id must be in the operators array for this to work
- */
-
 const Discord = require('discord.js')
 const util = require('minecraft-server-util')
 const fs = require('fs')
@@ -76,7 +55,8 @@ client.on('messageCreate', msg => {
                 } else {
                     msg.channel.send('ERROR: You cannot stop the server when there are players online.')
                 }
-            }).catch(() => {
+            }).catch(error => {
+                console.warn(`Error while stopping server. This is most likely because the server is not running: ${error.message}`)
                 msg.channel.send(`The server is not running. Use ${config.prefix}start to start it`)
             })
             break
@@ -87,8 +67,9 @@ client.on('messageCreate', msg => {
                     rcon.execute(msg.content).then(response => {
                         msg.channel.send(`SERVER: ${response}`)
                     })
-                }).catch(() => {
+                }).catch(error => {
                     msg.channel.send(`The server is offline. Use ${config.prefix}start to start it`)
+                    console.warn(`Error while executing command: ${error.message}`)
                 })
             } else {
                 msg.channel.send("You do not have permission to send commands to the server. If you are the owner of the server, add your user id to the operators array in the config.js file.")
@@ -104,7 +85,7 @@ client.on('messageCreate', msg => {
                     fs.mkdirSync(backup_directory)
                 } catch (error) {
                     if (error.code !== 'EEXIST') {
-                        console.log(error)
+                        console.warn(`Error while making backup directories: ${error.message}`)
                         msg.channel.send('The backup directory could not be created. You most likely did not set the server directory properly or need to run the bot as an administrator.')
                     }
                 }
@@ -113,7 +94,7 @@ client.on('messageCreate', msg => {
                     try {
                         fs.cpSync(dir, backup_directory, { recursive: true, force: true })
                     } catch (error) {
-                        console.log(error)
+                        console.warn(`Error while backing up files: ${error.message}`)
                         msg.channel.send(`The directory ${dir} could not be backed up. If this persists try editing the dirs_to_backup variable or run the bot as an administrator.`)
                     }
                 })
